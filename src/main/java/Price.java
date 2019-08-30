4edede8b2754daa54b02a153d00512a0837c5faa
Eason Li <lijinghua@odc.cmbchina.cn>
2019-08-30 16:28:07 +0800

    Add tests for calculate points

--- src/main/java/Price.java
+++ src/main/java/Price.java
@@ -1,4 +1,5 @@
 public abstract class Price {
+    protected static int ONE_DAY = 1;
     protected static int TWO_DAYS = 2;
     protected static int THREE_DAYS = 3;
     protected static int ONT_POINT = 1;
@@ -7,9 +8,6 @@ public abstract class Price {
     protected static double TWO_$ = 2;
     protected static double THREE_$_EVERY_DAY = 3;
     protected static double ONE_POINT_FIVE_$_EVERT_DAY = 1.5;
-    
-    
-    
     
     public abstract int getPriceCode();
     
--- src/test/java/NewReleasePriceTest.java
+++ src/test/java/NewReleasePriceTest.java
@@ -12,4 +12,11 @@ public class NewReleasePriceTest {
         assertEquals(6, result, 0);
     }
     
+    @Test
+    public void should_return_2_points_when_rental_days_2() {
+        int result = newReleasePrice.getFrequentRenterPoints(2);
+        
+        assertEquals(result, 2);
+    }
+    
 }
